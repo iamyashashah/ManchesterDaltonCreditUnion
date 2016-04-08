11dd76b30d12cf6f055c518e0ce2d2759270d8b7
Sam Mazur <mazur.sam@gmail.com>
2016-04-08 14:16:41 -0400

    changes

---
+++ src/customcode/AccountDetails.java
@@ -0,0 +1,71 @@
+package customcode;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Manaccount;
+
+/**
+ * Servlet implementation class AccountDetails
+ */
+@WebServlet("/AccountDetails")
+public class AccountDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AccountDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		doPost(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		HttpSession session = request.getSession();
+
+		if (request.getParameter("option").equals("2")) {
+
+			long accountid = Long.parseLong((String) session.getAttribute("accountid"));
+
+			// Studentgrade st = null;
+			List<Manaccount> accounts = null;
+
+			accounts = ProcessAccount.getAccountById(accountid);
+			double balance = ProcessTransaction.getBalance(accountid);
+
+			//
+			session.setAttribute("accountdetails", accounts);
+			session.setAttribute("balance", balance);
+
+			request.getRequestDispatcher("/accountdetails.jsp").forward(request, response);
+
+		}
+	}
+}
