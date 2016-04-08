5f976064cf8bafe4542278249badc30755f50d6f
Chenye Yin <elizebethyin@gmail.com>
2016-04-08 13:58:17 -0400

    Process and test

--- UnitTest/TestUser.java
+++ UnitTest/TestUser.java
@@ -1,4 +1,19 @@
+import static org.junit.Assert.* ;
+
+import org.junit.Test;
+
+import model.Manuser;
+import processor.ProcessUser;
 
 public class TestUser {
+	
+	@Test
+	   public void test_loginuser() {
+		Manuser user=new Manuser();
+	      System.out.println("Test if table has been sucessfully imported") ;
+	      user=ProcessUser.userLogin("user1", "password");
+	     // Subscription S = new Subscription(200,2) ;
+	      assertEquals(user.getUserid(),1) ;
+	   }
 
 }
--- src/processor/ProcessUser.java
+++ src/processor/ProcessUser.java
@@ -1,6 +1,5 @@
 package processor;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
