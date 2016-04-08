ab2ff29a6f563dca6df2d7b4bafe81acbc96a9f8
Sam Mazur <mazur.sam@gmail.com>
2016-04-08 14:51:47 -0400

    balance

--- UnitTest/TestTransaction.java
+++ UnitTest/TestTransaction.java
@@ -1,7 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.Manuser;
+import processor.ProcessTransaction;
+
 
 public class TestTransaction {
 public void seeTransaction(){
 	
 	
 }
+@Test
+public void test_balance() {
+	
+	double balance = 0;
+	
+	
+   System.out.println("Test if table has been sucessfully imported") ;
+   
+   balance = ProcessTransaction.getBalance(1);
+  
+
+   assertTrue(balance == 62.43) ;
+}
+
+
 }
--- src/processor/ProcessTransaction.java
+++ src/processor/ProcessTransaction.java
@@ -1,6 +1,50 @@
 package processor;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import model.DBUtil;
+import model.Mantransaction;
+import model.Mantransaction;
+
 public class ProcessTransaction {
+	
 public ProcessTransaction(){}
+
+public static double getBalance(long _accountid) {
+	//
+	EntityManager em = DBUtil.getEmFactory().createEntityManager();
+	String qString = "Select sum(t.amount) from Mantransaction t where t.manaccount.accountid = :accountid";
+    Mantransaction mt = new Mantransaction();
+
+	Query q = em.createQuery(qString, model.Mantransaction.class);
+	q.setParameter("accountid", _accountid);
+	double balance = 0;
+
+		
+	try {
+		//		trans.begin();
+		System.out.println("accountid = " + _accountid);
+		balance =  (double) q.getSingleResult();
+
+		System.out.println("balance = " + balance );
+		//			trans.commit();
+
+	} catch (Exception e) {
+		System.out.println(e);
+		//			trans.rollback();
+	} finally {
+
+		em.close();
+		System.out.println("Finished");
+		return balance;
+	}
+
+
+}
+
 
 }
